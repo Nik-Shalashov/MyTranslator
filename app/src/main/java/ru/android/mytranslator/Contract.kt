4bd4b8994a33bea25d074976a432b053faa0e55b
Nik-Shalashov <monitor1@inbox.ru>
2021-11-01 23:34:45 +0700

    ВЫнес в отдельный класс интерфейсы для реализации паттерна MVP

---
+++ app/src/main/java/ru/android/mytranslator/Contract.kt
@@ -0,0 +1,31 @@
+package ru.android.mytranslator
+
+import io.reactivex.Observable
+import ru.android.mytranslator.model.data.AppState
+
+class Contract {
+
+    interface View {
+        fun renderData(appState: AppState)
+    }
+
+
+    interface Presenter<T : AppState, V : View> {
+        fun attachView(view: V)
+        fun detachView(view: V)
+        fun getData(word: String, isOnline: Boolean)
+    }
+
+    interface Interactor<T> {
+        fun getData(word: String, fromRemoteSource: Boolean): Observable<T>
+    }
+
+
+    interface Repository<T> {
+        fun getData(word: String): Observable<T>
+    }
+
+    interface DataSource<T> {
+        fun getData(word: String): Observable<T>
+    }
+}
