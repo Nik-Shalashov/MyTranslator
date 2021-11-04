c0061b98a0d58cf05d405f7f56ab77c7990e19d3
Nik-Shalashov <monitor1@inbox.ru>
2021-11-04 21:36:27 +0700

    Добавил dependency injection

--- app/src/main/java/ru/android/mytranslator/presenter/MainInteractor.kt
+++ app/src/main/java/ru/android/mytranslator/presenter/MainInteractor.kt
@@ -2,19 +2,23 @@ package ru.android.mytranslator.presenter
 
 import io.reactivex.Observable
 import ru.android.mytranslator.Contract
+import ru.android.mytranslator.di.NAME_LOCAL
+import ru.android.mytranslator.di.NAME_REMOTE
 import ru.android.mytranslator.model.data.AppState
 import ru.android.mytranslator.model.data.DataModel
+import javax.inject.Inject
+import javax.inject.Named
 
-class MainInteractor(
-    private val remoteRepository: Contract.Repository<List<DataModel>>,
-    private val localRepository: Contract.Repository<List<DataModel>>
+class MainInteractor @Inject constructor(
+    @Named(NAME_REMOTE) val repositoryRemote: Contract.Repository<List<DataModel>>,
+    @Named(NAME_LOCAL) val repositoryLocal: Contract.Repository<List<DataModel>>
 ) : Contract.Interactor<AppState> {
 
     override fun getData(word: String, fromRemoteSource: Boolean): Observable<AppState> {
         return if (fromRemoteSource) {
-            remoteRepository.getData(word).map { AppState.Success(it) }
+            repositoryRemote
         } else {
-            localRepository.getData(word).map { AppState.Success(it) }
-        }
+            repositoryLocal
+        }.getData(word).map { AppState.Success(it) }
     }
 }
--- app/src/main/java/ru/android/mytranslator/view/main/MainActivity.kt
+++ app/src/main/java/ru/android/mytranslator/view/main/MainActivity.kt
@@ -3,63 +3,81 @@ package ru.android.mytranslator.view.main
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import dagger.android.AndroidInjection
 import ru.android.mytranslator.Contract
 import ru.android.mytranslator.R
 import ru.android.mytranslator.databinding.ActivityMainBinding
 import ru.android.mytranslator.model.data.AppState
 import ru.android.mytranslator.model.data.DataModel
+import ru.android.mytranslator.presenter.MainInteractor
 import ru.android.mytranslator.presenter.MainPresenterImpl
+import ru.android.mytranslator.utils.network.isOnline
 import ru.android.mytranslator.view.base.BaseActivity
+import ru.android.mytranslator.view_model.MainViewModel
+import javax.inject.Inject
 
-class MainActivity : BaseActivity<AppState>() {
+class MainActivity : BaseActivity<AppState, MainInteractor>() {
+
+    @Inject
+    internal lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private lateinit var binding: ActivityMainBinding
-
-    private var adapter: MainAdapter? = null
+    override lateinit var model: MainViewModel
+    private val adapter: MainAdapter by lazy { MainAdapter(onListItemClickListener) }
+    private val fabClickListener: View.OnClickListener =
+        View.OnClickListener {
+            val searchDialogFragment = SearchDialogFragment.newInstance()
+            searchDialogFragment.setOnSearchClickListener(onSearchClickListener)
+            searchDialogFragment.show(supportFragmentManager, BOTTOM_SHEET_FRAGMENT_DIALOG_TAG)
+        }
     private val onListItemClickListener: MainAdapter.OnListItemClickListener =
         object : MainAdapter.OnListItemClickListener {
             override fun onItemClick(data: DataModel) {
                 Toast.makeText(this@MainActivity, data.text, Toast.LENGTH_SHORT).show()
             }
         }
-
-    override fun createPresenter(): Contract.Presenter<AppState, Contract.View> {
-        return MainPresenterImpl()
-    }
+    private val onSearchClickListener: SearchDialogFragment.OnSearchClickListener =
+        object : SearchDialogFragment.OnSearchClickListener {
+            override fun onClick(searchWord: String) {
+                isNetworkAvailable = isOnline(applicationContext)
+                if (isNetworkAvailable) {
+                    model.getData(searchWord, isNetworkAvailable)
+                } else {
+                    showNoInternetConnectionDialog()
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.searchFab.setOnClickListener {
-            val searchDialogFragment = SearchDialogFragment.newInstance()
-            searchDialogFragment.setOnSearchClickListener(object :
-                SearchDialogFragment.OnSearchClickListener {
-                override fun onClick(searchWord: String) {
-                    presenter.getData(searchWord, true)
-                }
-            })
-            searchDialogFragment.show(supportFragmentManager, BOTTOM_SHEET_FRAGMENT_DIALOG_TAG)
-        }
+
+        model = viewModelFactory.create(MainViewModel::class.java)
+        model.subscribe().observe(this@MainActivity, Observer<AppState> { renderData(it) })
+
+        binding.searchFab.setOnClickListener(fabClickListener)
+        binding.mainActivityRecyclerview.layoutManager = LinearLayoutManager(applicationContext)
+        binding.mainActivityRecyclerview.adapter = adapter
     }
 
     override fun renderData(appState: AppState) {
         when (appState) {
             is AppState.Success -> {
-                val dataModel = appState.data
-                if (dataModel == null || dataModel.isEmpty()) {
-                    showErrorScreen(getString(R.string.empty_server_response_on_success))
+                showViewWorking()
+                val data = appState.data
+                if (data.isNullOrEmpty()) {
+                    showAlertDialog(
+                        getString(R.string.dialog_tittle_sorry),
+                        getString(R.string.empty_server_response_on_success)
+                    )
                 } else {
-                    showViewSuccess()
-                    if (adapter == null) {
-                        binding.mainActivityRecyclerview.layoutManager =
-                            LinearLayoutManager(applicationContext)
-                        binding.mainActivityRecyclerview.adapter =
-                            MainAdapter(onListItemClickListener, dataModel)
-                    } else {
-                        adapter!!.setData(dataModel)
-                    }
+                    adapter.setData(data)
                 }
             }
             is AppState.Loading -> {
@@ -74,35 +92,18 @@ class MainActivity : BaseActivity<AppState>() {
                 }
             }
             is AppState.Error -> {
-                showErrorScreen(appState.error.message)
+                showViewWorking()
+                showAlertDialog(getString(R.string.error_stub), appState.error.message)
             }
         }
     }
 
-    private fun showErrorScreen(error: String?) {
-        showViewError()
-        binding.errorTextview.text = error ?: getString(R.string.undefined_error)
-        binding.reloadButton.setOnClickListener {
-            presenter.getData("hi", true)
-        }
-    }
-
-    private fun showViewSuccess() {
-        binding.successLinearLayout.visibility = View.VISIBLE
+    private fun showViewWorking() {
         binding.loadingFrameLayout.visibility = View.GONE
-        binding.errorLinearLayout.visibility = View.GONE
     }
 
     private fun showViewLoading() {
-        binding.successLinearLayout.visibility = View.GONE
         binding.loadingFrameLayout.visibility = View.VISIBLE
-        binding.errorLinearLayout.visibility = View.GONE
-    }
-
-    private fun showViewError() {
-        binding.successLinearLayout.visibility = View.GONE
-        binding.loadingFrameLayout.visibility = View.GONE
-        binding.errorLinearLayout.visibility = View.VISIBLE
     }
 
     companion object {
