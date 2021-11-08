5e7c2046cccb1e0e78aee049f8ddc3b2d1f9702c
Nik-Shalashov <monitor1@inbox.ru>
2021-11-08 22:23:42 +0700

    убрал ненужные классы, использовал coroutins

--- app/src/main/java/ru/android/mytranslator/Contract.kt
+++ app/src/main/java/ru/android/mytranslator/Contract.kt
@@ -1,29 +1,16 @@
 package ru.android.mytranslator
 
-import io.reactivex.Observable
-import ru.android.mytranslator.model.data.AppState
-
 class Contract {
 
-    interface View {
-        fun renderData(appState: AppState)
-    }
-
-    interface Presenter<T : AppState, V : View> {
-        fun attachView(view: V)
-        fun detachView(view: V)
-        fun getData(word: String, isOnline: Boolean)
-    }
-
     interface Interactor<T> {
-        fun getData(word: String, fromRemoteSource: Boolean): Observable<T>
+        suspend fun getData(word: String, fromRemoteSource: Boolean): T
     }
 
     interface Repository<T> {
-        fun getData(word: String): Observable<T>
+        suspend fun getData(word: String): T
     }
 
     interface DataSource<T> {
-        fun getData(word: String): Observable<T>
+        suspend fun getData(word: String): T
     }
 }
