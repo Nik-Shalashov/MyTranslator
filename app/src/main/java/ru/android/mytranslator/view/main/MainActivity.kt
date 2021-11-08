2fc50037b96336bf3a26185aafdc26e5b0896a7c
Nik-Shalashov <monitor1@inbox.ru>
2021-11-08 22:21:25 +0700

    заинжектил зависимости koin

--- app/src/main/java/ru/android/mytranslator/view/main/MainActivity.kt
+++ app/src/main/java/ru/android/mytranslator/view/main/MainActivity.kt
@@ -3,26 +3,17 @@ package ru.android.mytranslator.view.main
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import dagger.android.AndroidInjection
-import ru.android.mytranslator.Contract
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.android.mytranslator.R
 import ru.android.mytranslator.databinding.ActivityMainBinding
 import ru.android.mytranslator.model.data.AppState
 import ru.android.mytranslator.model.data.DataModel
-import ru.android.mytranslator.presenter.MainInteractor
-import ru.android.mytranslator.presenter.MainPresenterImpl
 import ru.android.mytranslator.utils.network.isOnline
 import ru.android.mytranslator.view.base.BaseActivity
 import ru.android.mytranslator.view_model.MainViewModel
-import javax.inject.Inject
 
 class MainActivity : BaseActivity<AppState, MainInteractor>() {
-
-    @Inject
-    internal lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private lateinit var binding: ActivityMainBinding
     override lateinit var model: MainViewModel
@@ -52,18 +43,11 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
-
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        model = viewModelFactory.create(MainViewModel::class.java)
-        model.subscribe().observe(this@MainActivity, Observer<AppState> { renderData(it) })
-
-        binding.searchFab.setOnClickListener(fabClickListener)
-        binding.mainActivityRecyclerview.layoutManager = LinearLayoutManager(applicationContext)
-        binding.mainActivityRecyclerview.adapter = adapter
+        initViewModel()
+        initViews()
     }
 
     override fun renderData(appState: AppState) {
@@ -73,7 +57,7 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
                 val data = appState.data
                 if (data.isNullOrEmpty()) {
                     showAlertDialog(
-                        getString(R.string.dialog_tittle_sorry),
+                        getString(R.string.dialog_title_sorry),
                         getString(R.string.empty_server_response_on_success)
                     )
                 } else {
@@ -96,6 +80,21 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
                 showAlertDialog(getString(R.string.error_stub), appState.error.message)
             }
         }
+    }
+
+    private fun initViewModel() {
+        if (binding.mainActivityRecyclerview.adapter != null) {
+            throw IllegalStateException("The ViewModel should be initialised first")
+        }
+        val viewModel: MainViewModel by viewModel()
+        model = viewModel
+        model.subscribe().observe(this@MainActivity, { renderData(it) })
+    }
+
+    private fun initViews() {
+        binding.searchFab.setOnClickListener(fabClickListener)
+        binding.mainActivityRecyclerview.layoutManager = LinearLayoutManager(applicationContext)
+        binding.mainActivityRecyclerview.adapter = adapter
     }
 
     private fun showViewWorking() {
